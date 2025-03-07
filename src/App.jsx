9f3867ae4bb7c3d029ebe06201718182482c703d
Aldo <aldo.leka@live.com>
2025-03-07 11:37:15 +0100

    Lecture 159 - Select a project

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import {useState} from "react";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
     const [projectsState, setProjectsState] = useState({
         selectedProjectId: undefined,
         projects: []
     });
+
+    function handleSelectProject(id) {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: id
+            };
+        });
+    }
 
     function handleStartAddProject() {
         setProjectsState(prevState => {
@@ -42,7 +52,9 @@ function App() {
         });
     }
 
-    let content;
+    const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+    let content = <SelectedProject project={selectedProject} />;
+
     if (projectsState.selectedProjectId === null) {
         content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
     }
@@ -55,6 +67,7 @@ function App() {
             <ProjectsSidebar
                 onStartAddProject={handleStartAddProject}
                 projects={projectsState.projects}
+                onSelectProject={handleSelectProject}
             />
             {content}
         </main>
